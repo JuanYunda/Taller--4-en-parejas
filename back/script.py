4c427c821ac1f25aa4b5e6abbd53e951a855e61c
Juan Esteban Mazuera Yunda 2043008 <juan.yunda@correounivalle.edu.co>
2024-05-07 22:59:31 -0500

    Busqueda de los libros desde la pagina web

--- back/script.py
+++ back/script.py
@@ -2,24 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-books = []
+def consultar_precio():
+    books = []
 
-for i in range(1,5):
-  url = f"https://books.toscrape.com/catalogue/page-{i}.html"
-  response = requests.get(url)
-  response = response.content
-  soup = BeautifulSoup(response, 'html.parser')
-  ol = soup.find('ol')
-  articles = ol.find_all('article', class_='product_pod')
-  for article in articles:
-    image = article.find('img')
-    title = image.attrs['alt']
-    starTag = article.find('p')
-    star = starTag['class'][1]
-    price = article.find('p', class_='price_color').text
-    price = float(price[1:])
-    books.append([title, star, price])
-    
+    for i in range(1,5):
+        url = f"https://books.toscrape.com/catalogue/page-{i}.html"
+        response = requests.get(url)
+        response = response.content
+        soup = BeautifulSoup(response, 'html.parser')
+        ol = soup.find('ol')
+        articles = ol.find_all('article', class_='product_pod')
+        for article in articles:
+            image = article.find('img')
+            title = image.attrs['alt']
+            starTag = article.find('p')
+            star = starTag['class'][1]
+            price = article.find('p', class_='price_color').text
+            price = float(price[1:])
+            books.append([title, star, price])
 
-df = pd.DataFrame(books, columns=['Title', 'Star Rating', 'Price'])
-df.to_csv('books.csv')
+    df = pd.DataFrame(books, columns=['Title', 'Star Rating', 'Price'])
+    listaDeLibros = df.to_html()
+
+    # Guardar el HTML en un archivo
+    with open('interfaz/dataframe.html', 'w') as f:
+        f.write(listaDeLibros)
+
+# Llama a la función para generar el HTML
+consultar_precio()
